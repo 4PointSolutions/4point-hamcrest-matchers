58054d82702e706d58d2befeba810925195b784d
Rob McDougall <rmcdougawww@rogers.com>
2023-11-25 10:40:40 -0500

    Added HasCause and HasCauseMatching methods.

--- src/test/java/com/_4point/testing/matchers/javalang/ExceptionMatchersTest.java
+++ src/test/java/com/_4point/testing/matchers/javalang/ExceptionMatchersTest.java
@@ -1,11 +1,14 @@
 package com._4point.testing.matchers.javalang;
 
-import static com._4point.testing.matchers.javalang.ExceptionMatchers.exceptionMsgContainsAll;
+import static com._4point.testing.matchers.javalang.ExceptionMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat; 
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class ExceptionMatchersTest {
 
@@ -88,5 +91,64 @@ class ExceptionMatchersTest {
 		assertThat(msg, containsString("Exception message was null."));
 	}
 
+	private static final String CAUSE_EXCEPTION_MSG = "Cause Exception";
+	private static final Exception CAUSE_EXCEPTION = new IllegalStateException(CAUSE_EXCEPTION_MSG);
+	private static final Exception TEST_EXCEPTION = new IllegalStateException("Test Exception", CAUSE_EXCEPTION);
+	private static final Exception NON_CAUSE_EXCEPTION = new IllegalStateException("Non-cause Exception");
+
+	@Test
+	void testHasCause_Match() {
+		assertThat(TEST_EXCEPTION,hasCause(CAUSE_EXCEPTION));
+	}
+
+	@Test
+	void testHasCause_Mismatch() {
+		AssertionError ex = assertThrows(AssertionError.class, ()->assertThat(TEST_EXCEPTION,hasCause(NON_CAUSE_EXCEPTION)));
+		
+		String msg = ex.getMessage();
+		assertNotNull(msg);
+		assertThat(msg, containsString("an exception with cause"));		
+	}
+	
+	
+	private enum ExceptionCauseMatchesScenario {
+		ONE_EXCEPTION(
+				hasCauseMatching(sameInstance(CAUSE_EXCEPTION)), 
+				hasCauseMatching(sameInstance(NON_CAUSE_EXCEPTION))
+				),
+		TWO_EXCEPTIONS(
+				hasCauseMatching(exceptionMsgContainsAll(CAUSE_EXCEPTION_MSG), sameInstance(CAUSE_EXCEPTION)),
+				hasCauseMatching(exceptionMsgContainsAll(CAUSE_EXCEPTION_MSG), sameInstance(NON_CAUSE_EXCEPTION))
+				),
+		THREE_EXCEPTIONS(
+				hasCauseMatching(sameInstance(CAUSE_EXCEPTION), exceptionMsgContainsAll(CAUSE_EXCEPTION_MSG), sameInstance(CAUSE_EXCEPTION)),
+				hasCauseMatching(sameInstance(NON_CAUSE_EXCEPTION), exceptionMsgContainsAll(CAUSE_EXCEPTION_MSG), sameInstance(NON_CAUSE_EXCEPTION))
+			),
+		;
+		
+		private final Matcher<Throwable> passingTest;
+		private final Matcher<Throwable> failingTest;
+
+		private ExceptionCauseMatchesScenario(Matcher<Throwable> passingTest, Matcher<Throwable> failingTest) {
+			this.passingTest = passingTest;
+			this.failingTest = failingTest;
+		}
+	}
+	
+	@ParameterizedTest
+	@EnumSource
+	void testHasCauseMatching_Match(ExceptionCauseMatchesScenario scenario) {
+		assertThat(TEST_EXCEPTION, scenario.passingTest);
+	}
+
+	@ParameterizedTest
+	@EnumSource
+	void testHasCauseMatching_Mismatch(ExceptionCauseMatchesScenario scenario) {
+		AssertionError ex = assertThrows(AssertionError.class, ()->assertThat(TEST_EXCEPTION, scenario.failingTest));
+		
+		String msg = ex.getMessage();
+		assertNotNull(msg);
+		assertThat(msg, containsString("an exception with cause"));		
+	}
 
 }
